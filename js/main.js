b56862cc4930035969f2354e3bd89e3969da5fbb
Camila Micaela Urquizo Suca <curquizo@belatrix.com>
2019-11-14 18:06:15 -0500

    Second

--- js/main.js
+++ js/main.js
@@ -13,8 +13,10 @@ console.log(dots[0])
 for(let element of dots){
 	element.addEventListener("click",() => {
 		hideSlide();
+		showborder();
 		document.getElementById(element.dataset.slideName).classList.remove('display--off')
 		document.getElementById(element.dataset.slideName).classList.add('display--on')
+		document.getElementById(element.dataset.slideName).classList.add('border-botton')
 	})
 }
 
@@ -23,5 +25,19 @@ function hideSlide() {
 	for(let element of items) {
 		element.classList.remove('.display--on')
 		element.classList.add('display--off')
+		
+	}
+}
+function showborder() {
+	let items = document.getElementsByClassName("slide-nav__dot")
+	for(let element of items) {
+		element.classList.remove('border-botton')
+		element.addEventListener("click",() => {
+
+			element.classList.add('border-botton')
+		
+		})
+
+		
 	}
 }
